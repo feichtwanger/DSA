3997cd100826e36aeca29b4015d72d770a3070bc
Feichtwanger <feichtwanger7@gmail.com>
2024-04-19 21:46:51 +0300

    add README

--- c/stack.c
+++ c/stack.c
@@ -8,17 +8,18 @@
 int count = 0;
 
 // Creating a stack
-struct stack {
+struct stack
+{
   int items[MAX];
   int top;
 };
 typedef struct stack st;
 
-void createEmptyStack(st *s) {
+void createEmptyStack(st *s){
   s->top = -1;
 }
 
-// Check if the stack is full
+// Chech if the stack is full 
 int isfull(st *s) {
   if (s->top == MAX - 1)
     return 1;
@@ -26,17 +27,18 @@ int isfull(st *s) {
     return 0;
 }
 
-// Check if the stack is empty
-int isempty(st *s) {
-  if (s->top == -1)
+// Chech if the stack is empty
+int isempty(st *s){
+  if (s -> top == -1)
     return 1;
-  else
+  else 
     return 0;
 }
 
+
 // Add elements into stack
-void push(st *s, int newitem) {
-  if (isfull(s)) {
+void push(st *s, int newitem){
+  if(isfull(s)) {
     printf("STACK FULL");
   } else {
     s->top++;
@@ -45,29 +47,28 @@ void push(st *s, int newitem) {
   count++;
 }
 
-// Remove element from stack
-void pop(st *s) {
-  if (isempty(s)) {
-    printf("\n STACK EMPTY \n");
+//Remove element from stack
+void pop(st *s){
+  if (isempty(s)){
+    printf("\n Stack EMPTY \n");
   } else {
-    printf("Item popped= %d", s->items[s->top]);
+    printf("Item popped = %d", s->items[s->top]);
     s->top--;
   }
   count--;
   printf("\n");
 }
-
-// Print elements of stack
-void printStack(st *s) {
+// Print element of stack
+void printStack(st *s){
   printf("Stack: ");
-  for (int i = 0; i < count; i++) {
-    printf("%d ", s->items[i]);
+  for (int i = 0; i < count; i++){
+    printf("%d", s->items[i]);
   }
   printf("\n");
 }
 
 // Driver code
-int main() {
+int main(){
   int ch;
   st *s = (st *)malloc(sizeof(st));
 
